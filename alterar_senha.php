1c16cb99de5d4fdb52349dbf6c23e4fd9e91fa50
Caio Vinicius <caioviniaires@gmail.com>
2025-08-14 19:52:53 -0300

    add
    add: adição de alterar senha

--- alterar_senha.php
+++ alterar_senha.php
@@ -4,15 +4,94 @@ require_once 'conexao.php';
 
 //GARANTE QUE O USUARIO ESTEJA LOGADO
 
-if(isset(['id_usuario'])){
-    echo"<script></script>";
+if (isset($_SESSION['id_usuario'])) {
+    echo "<script>('Acesso negado');window.location.href='login.php';</script>";
+
     exit();
-
-
-
 
 }
 
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id_usuario = $_SESSION['id_usuario'];
+    $nova_senha = $_SESSION['nova_senha'];
+    $confirmar_senha = $_SESSION['confirmar_senha'];
+
+    if ($nova_senha !== $confirmar_senha) {
+        echo "<script>alert('As senhas não coincidem')</script>";
+    } elseif (strlen($nova_senha) < 8) {
+        echo "<script>alert('A senha deve ter pelo menos 8 caracteres')</script>";
+    }
+} else {
+    $senha_hash = password_hash($nova_senha, $PASSWORD_DEFAULT);
+
+    //Atualiza a senha e remove q status de temporaria:
+
+    $sql = "UPDATE usuario SET senha = :senha,senha_temporaria = FALSE WHERE id_usuario = :id";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':senha', $senha_hash);
+    $stmt->bindParam(':id', $id_usuario);
+
+    if ($stmt->execute()) {
+
+        session_destroy(); // Finaliza a sessão
+        echo "<script>alert('Senha alterada com sucesso! Faça login novamente');window.locate:login.php</script>";
+    } else {
+        echo "<script>alert('Email não encontrado!');window.locate:login.php</script>";
+
+    }
+
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.cssu">
+    <title>Recuperação de senha</title>
+</head>
+
+<body>
+    <h2>Alterar senha</h2>
+    <p>Olá, <strong><?php echo $_SESSION['usuario']; ?></strong>.Digite sua nova senha abaixo:</p>
+
+    <form action="alterar_senha.php" method="post">
+        <label for="nova_senha">Nova Senha</label>
+        <input type="password" id="nova_senha" name="nova_senha" required>
+
+        <label for="confirmar_senha">Confirmar senha</label>
+        <input type="password" id="confirmar_senha" name="confirmar_senha" required>
+
+        <label for="checkbox" onclick="mostrarSenha()">Mostrar senha</label>
+        <button type="button">Salvar senha</button>
+
+    </form>
+
+    <script>
+        function mostrarSenha() {
+            var senha1 = document.getElementById('nova_senha');
+            var senha2 = document.getElementById('confirmar_senha');
+            var tipo = senha1.Type === "password" ? "text: "password"";
+
+            senha1.type = tipo;
+            senha2.type = tipo;
+
+
+        }
+
+
+
+    </script>
+
+
+
+
+
+</body>
+
+</html>
